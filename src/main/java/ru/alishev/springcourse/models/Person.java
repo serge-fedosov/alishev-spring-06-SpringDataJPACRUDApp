c5514ddaaf2568ac360725812213ab5ec1014a2e
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-03 00:41:15 +0400

    08/02 add item

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 @Table(name = "Person")
@@ -28,6 +29,9 @@ public class Person {
     @Email(message = "Email should be valid")
     @Column(name = "email")
     private String email;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
 
     public Person() {
 
