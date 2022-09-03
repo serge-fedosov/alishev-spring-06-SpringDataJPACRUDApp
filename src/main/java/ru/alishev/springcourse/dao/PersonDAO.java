0793d8dfdd6dd7723e937282e1bcecb1a347efb7
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-03 10:37:13 +0400

    09/65 N + 1 problem solved - only unique person

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -7,7 +7,9 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.alishev.springcourse.models.Person;
 
 import javax.persistence.EntityManager;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class PersonDAO {
@@ -32,7 +34,7 @@ public class PersonDAO {
 //        }
 
         // SQL: A LEFT JOIN B -> Результирующая объединенная таблица
-        List<Person> people = session.createQuery("select p from Person p LEFT JOIN FETCH p.items").getResultList();
+        Set<Person> people = new HashSet<Person>(session.createQuery("select p from Person p LEFT JOIN FETCH p.items").getResultList());
 
         for (Person person : people) {
             System.out.println("Person: " + person.getName() + " has: " + person.getItems());
--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Person")
@@ -119,6 +120,21 @@ public class Person {
 
     public void setMood(Mood mood) {
         this.mood = mood;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && age == person.age && Objects.equals(name, person.name) && Objects.equals(email, person.email) &&
+                Objects.equals(dateOfBirth, person.dateOfBirth) && Objects.equals(createdAt, person.createdAt) &&
+                mood == person.mood;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, email, dateOfBirth, createdAt, mood);
     }
 
     @Override
